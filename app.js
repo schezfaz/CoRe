c5eebb27f515851bf686a8986d6faa78ef4f91a2
Ben <lolitsaburrito@gmail.com>
2021-06-09 15:43:10 -0700

    Refactoring, course results are now returned in ascending order (by price)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var udemyRouter = require('./routes/udemy-api');
+var udemyRouter = require('./routes/udemy-api').router;
 
 var app = express();
 
--- routes/udemy-api.js
+++ routes/udemy-api.js
@@ -7,21 +7,39 @@ const bent = require('bent')
 const getJSON = bent('json')
 
 router.get('/', async (req, res, next) => {
-	console.log(req.query.search);
+	const result = await searchCourses(req.query.search);
 
-	if (!req.query.search) return res.send({error: "Search query required (i.e. ...?search=data+structures)"})
+	if (result.error) {
+		res.status(result.status).send(result)
+	} else {
+		res.status(200).send(result);
+	}
+});
+
+const searchCourses = async (searchTerm) => {
+	if (!searchTerm) return {
+		error: "Search query required (i.e. ...?search=data+structures)",
+		status: 400
+	};
 	
-	let udemyRes = await getJSON(getQuery(req.query.search), {}, getHeaders())
+	let udemyRes = await getJSON(getQuery(searchTerm), {}, getHeaders())
 		.catch(e => {
 			console.log(`Error fetching Udemy courses: ${e}`);
-			res.send({ error: e });
+			return {
+				error: e,
+				status: 500
+			};
 		});
 	
-	console.log(udemyRes);
-	if (!udemyRes) return res.send({ error: "Result is undefined" });
+	if (!udemyRes) return {
+		error: "Result is undefined",
+		status: 500
+	};
 
-	res.status(200).send({results: udemyRes.results});
-})
+	return { // Have to sort locally because Udemy doesn't seem to do anything with the &ordering=price-low-to-high option.
+		results: udemyRes.results.sort((a, b) => a.price_detail.amount - b.price_detail.amount)
+	};
+}
 
 const getQuery = (searchTerm) => `https://www.udemy.com/api-2.0/courses/?page_size=10&search=${searchTerm}&ordering=price-low-to-high`
 const getHeaders = () => {
@@ -34,4 +52,7 @@ const getHeaders = () => {
 	return headers;
 }
 
-module.exports = router;
+module.exports = {
+	router,
+	searchCourses
+};
