17728875fea5d3b664db4a4db54e817cef36b8a2
unknown <schezeenf10@gmail.com>
2021-06-10 12:41:02 +0530

    asciidoctor sample integration

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ app.post('/courseQuery', (req,res)=>{
   res.json({'courseQuery' : course});
 })
 
+const asciidoctor = require('asciidoctor')() ;
+const content = 'http://asciidoctor.org[*Asciidoctor*] ' +
+  'running on https://opalrb.com[_Opal_] ' +
+  'brings AsciiDoc to Node.js!';
+const html = asciidoctor.convert(content) ;
+console.log(html);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
