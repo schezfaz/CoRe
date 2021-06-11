5d25b37f2ebdc236841e9e26f7bcde6f36d6c4b6
unknown <schezeenf10@gmail.com>
2021-06-11 11:24:19 +0530

    basic ui layout

--- frontend/src/Components/Landing.js
+++ frontend/src/Components/Landing.js
@@ -1,15 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import stylex from "@ladifire-opensource/stylex";
 
-
 export default function Landing() {
     const styles = stylex.create({
         root: {
-            backgroundColor: "#FF0000"
+            color: "black",
+            padding: "10px"
         },
+
+        core: {
+            fontWeight: 700
+        },
+
+        inputGroup: {
+            width: "40%",
+            height: "30px",
+            alignItems: "center", 
+            left: "45%"
+        },
+
+        searchButton: {
+            color: "#212529",
+            height: "30px",
+            backgroundColor: "#7cc",
+            borderColor: "#5bc2c2"
+        }
     });
 
-    const [name, setName] = useState('Schez');
+    const [courses, setCourses] = useState('Schez');
 
     useEffect(() => {
       fetch("http://localhost:5000/courseQuery",  {
@@ -23,17 +41,23 @@ export default function Landing() {
     }).then(function(data){
         return data.json() 
     }).then(function(data){
-        setName(data[0].title.stringify);
+        setCourses(data[0].title.stringify);
         console.log(data);
     })
     })
-        
-    
 
     return (
         <div className={stylex(styles.root)}>
-            <button>get name</button>
-            <h1>My name is {name}</h1>
+            <div>
+                <h1 className={stylex(styles.core)}>CoRe</h1>
+                <p>find the best course alternatives, where less is <i>core</i></p>
+            </div>
+
+            <div>
+                <input type="search" name="text" id="text"  className={stylex(styles.inputGroup)}
+                placeholder="enter a course name" aria-label="Search" aria-describedby="search-addon" />
+                <button className={stylex(styles.searchButton)} type="button" id="button" >search</button>
+            </div>
         </div>
     )
 }
